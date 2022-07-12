cf50bf7839673c3e547ad34b5ed2dbd58c2b87f9
Rosostolato <eduardo.rosostolato@gmail.com>
2022-07-12 17:39:14 -0300

    created animated stars

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,54 @@
 import { styled } from 'estrela';
+import logo from '../favicon.svg';
+import { getStarStyle } from './stars';
 
 const App = () => {
-  let name = 'Forasteiro';
+  let name = 'Adventurer';
 
   return (
-    <div class="app">
-      <h1>Welcome {name}!</h1>
+    <>
+      {/* For stars effect */}
+      <div id="stars1"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
 
-      <p>
-        This is your startup project. Please, visit Estrela{' '}
-        <a href="https://estrelajs.github.io/" target="_blank">
-          docs
-        </a>{' '}
-        to learn more about it.
-      </p>
+      {/* Template code */}
+      <div class="container">
+        <img src={logo} width="300" />
+        <h1>Welcome {name}!</h1>
+        <p>
+          This is your startup project. Please, visit Estrela{' '}
+          <a href="https://estrelajs.github.io/" target="_blank">
+            docs
+          </a>{' '}
+          to learn more about it.
+        </p>
 
-      <label for="name">Enter your name:</label>
-      <input id="name" bind={name$} autocomplete="off" />
-    </div>
+        <label for="name">Enter your name:</label>
+        <input id="name" bind={name$} autocomplete="off" />
+      </div>
+    </>
   );
 };
 
 export default styled(App)`
-  .app {
-    font-family: 'Roboto', sans-serif;
+  ${getStarStyle()}
+
+  .container {
     display: flex;
     flex-flow: column;
     align-items: center;
-    color: #555;
+    text-align: center;
 
     label {
       font-weight: bold;
       margin: 20px 0 10px;
+    }
+
+    img {
+      filter: brightness(0.75);
+      margin: 50px;
+      z-index: 1;
     }
 
     input {
